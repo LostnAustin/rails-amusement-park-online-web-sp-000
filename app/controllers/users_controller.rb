68dee804fc82574059d4c7698a1a21e22f13f9ee
Chad Lengefeld <chadlengefeld@gmail.com>
2021-05-20 05:14:46 +0000

    Automatically backed up by Learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,16 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
 
   def create
   end
+
+  private
+    def user_params
+      params.require(:user).permit(
+        :name, :height, :nausea, :happiness, :tickets, :height)
+        
+      )
+    end
 end
